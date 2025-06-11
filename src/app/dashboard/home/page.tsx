540ceebe9f5a5d14339bd542c18dd32d2b60177c
spiercer <snowpiercer2001@gmail.com>
2025-06-11 03:37:16 +0300

    top parts

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -1,21 +1,153 @@
-import { Logout } from "@/components/logout";
-import { Authenticated } from "@/lib/supabase/auth-components";
-import { createClient } from "@/lib/supabase/server";
+import Spotlight from "../../../components/spotlight";
+import { Button } from "../../../components/ui/button";
+import { Edit } from "lucide-react";
+import Image from "next/image";
+import profile from "../../../../public/profile.jpg";
+import { Progress } from "../../../components/ui/progress";
+import { ChartRadialText } from "../../../components/radial-chart";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../../../components/ui/card";
+import { SalesCard } from "./sales-card";
+import { ChartHalfRadialText } from "../../../components/half-radial-chart";
+import { cn } from "../../../lib/utils";
 
 export default async function Home() {
-  const supabase = await createClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center bg-blue-500">
-      <h1>Home</h1>
+    <div className="flex h-screen w-full flex-col items-start p-6">
+      <div className="flex min-h-0 w-full items-stretch gap-6">
+        <Spotlight className="w-1/2 flex-shrink-0">
+          <div className="flex flex-col gap-6">
+            <div className="flex items-center justify-between">
+              <p className="text-lg font-semibold">Personal info</p>
+              <Button variant="outline" className="text-[#094E87]">
+                <span>Edit Profile</span>
+                <Edit />
+              </Button>
+            </div>
 
-      <p>Welcome {user?.email}</p>
-      <Authenticated>
-        <p>You are authenticated</p>
-      </Authenticated>
-      <Logout />
+            <div className="flex gap-6">
+              <div className="relative h-[150px] w-[150px] rounded-lg">
+                <Image
+                  src={profile}
+                  alt="profile"
+                  fill
+                  className="rounded-lg object-cover object-top"
+                />
+              </div>
+              <div className="flex grow flex-col gap-6">
+                <div className="flex flex-col items-start">
+                  <p className="text-lg font-semibold">John Doe</p>
+                  <p className="text-md text-gray-500">Senior Associate</p>
+                </div>
+                <div className="flex flex-col gap-2">
+                  <div className="flex justify-between">
+                    <p className="text-md font-semibold">
+                      $7,350
+                      <span className="text-sm font-normal text-gray-500">
+                        {" of target $15,000"}
+                      </span>
+                    </p>
+                    <p className="text-md font-semibold">{"49%"}</p>
+                  </div>
+                  <Progress value={49} />
+                </div>
+              </div>
+            </div>
+
+            <div className="flex gap-4">
+              <CommissionCard
+                title="Commission"
+                range={{ min: 4200, max: 12000 }}
+                className="flex aspect-square w-full flex-col gap-0"
+              />
+
+              <SalesCard className="flex aspect-square w-full flex-col gap-0" />
+            </div>
+          </div>
+        </Spotlight>
+        <div className="grid min-h-0 w-1/2 flex-shrink-0 grid-cols-2 gap-6">
+          <GoalCard title="Weekly Goal" range={{ min: 850, max: 2500 }} />
+          <GoalCard title="Monthly Goal" range={{ min: 6200, max: 18000 }} />
+          <GoalCard title="Quarterly Goal" range={{ min: 15400, max: 45000 }} />
+          <GoalCard title="Yearly Goal" range={{ min: 78000, max: 150000 }} />
+        </div>
+      </div>
     </div>
+  );
+}
+
+function GoalCard({
+  title,
+  range,
+}: {
+  title: string;
+  range: {
+    min: number;
+    max: number;
+  };
+}) {
+  const value = Math.round((range.min / range.max) * 100);
+  return (
+    <Card className="flex min-h-0 w-full flex-col gap-0 overflow-hidden">
+      <CardHeader className="flex-shrink-0">
+        <CardTitle className="mx-auto text-center text-lg font-semibold">
+          {title}
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="min-h-0 flex-1 py-6">
+        <ChartHalfRadialText title={`${value}%`} value={value} />
+      </CardContent>
+      <CardFooter className="flex-shrink-0">
+        <CardTitle className="mx-auto text-center">
+          <p className="text-md font-semibold">
+            ${range.min}
+            <span className="text-sm font-normal text-gray-500">
+              {" out off $" + range.max}
+            </span>
+          </p>
+        </CardTitle>
+      </CardFooter>
+    </Card>
+  );
+}
+
+function CommissionCard({
+  title,
+  range,
+  className,
+}: {
+  title: string;
+
+  range: {
+    min: number;
+    max: number;
+  };
+  className?: React.HTMLAttributes<HTMLDivElement>["className"];
+}) {
+  const value = Math.round((range.min / range.max) * 100);
+  return (
+    <Card className={cn(className)}>
+      <CardHeader>
+        <CardTitle>
+          <p className="text-md font-semibold">
+            ${range.min}
+            <span className="text-sm font-normal text-gray-500">
+              {" of target $" + range.max}
+            </span>
+          </p>
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="grow py-6">
+        <ChartRadialText title={`${value}%`} value={value} />
+      </CardContent>
+      <CardFooter>
+        <CardTitle>{title}</CardTitle>
+      </CardFooter>
+    </Card>
   );
 }
