d534ef1e09f6f73932de92effabf82c20941676b
spiercer <snowpiercer2001@gmail.com>
2025-06-28 09:31:29 +0300

    added tabs layout

---
+++ src/app/dashboard/docs-management/layout.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Tabs, TabsLinkTrigger, TabsList } from "@/components/ui/tabs";
+import { Plus } from "lucide-react";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+export default function DocsManagementLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex h-full w-full flex-col items-start gap-6 p-6">
+      <div className="flex w-full justify-between">
+        <h1 className="text-2xl font-semibold">Documents Management</h1>
+        <DocsActions />
+      </div>
+      <TabBar />
+      <Card className="w-full grow py-0">
+        <CardContent className="grow">{children}</CardContent>
+      </Card>
+    </div>
+  );
+}
+
+function DocsActions() {
+  return (
+    <Button variant="default">
+      <Plus className="h-4 w-4" />
+      Add Document
+    </Button>
+  );
+}
+
+function TabBar() {
+  const path = usePathname();
+
+  return (
+    <Tabs defaultValue={path} className="w-[30%]">
+      <TabsList className="bg-card h-10 w-full">
+        <TabsLinkTrigger href="/dashboard/docs-management/news">
+          News
+        </TabsLinkTrigger>
+        <TabsLinkTrigger href="/dashboard/docs-management/contracts">
+          Contracts
+        </TabsLinkTrigger>
+        <TabsLinkTrigger href="/dashboard/docs-management/recruiting">
+          Recruiting
+        </TabsLinkTrigger>
+      </TabsList>
+    </Tabs>
+  );
+}
