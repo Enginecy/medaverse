81ef545f0260e9db8193c79167cfb842e0a193bd
Isaac Malak <isaacmalak123@gmail.com>
2025-07-17 20:45:25 +0300

    refactor: remove unnecessary destructuring from useQuery in ExportUsersButton

--- src/features/dashboard/user-management/components/users-exporting.tsx
+++ src/features/dashboard/user-management/components/users-exporting.tsx
@@ -11,7 +11,7 @@ import React from "react";
 
 export function ExportUsersButton() {
     const [ isLoading , setIsLoading ] = React.useState(false);
-  const { } = useQuery({
+useQuery({
     queryKey: ["users_export"],
     queryFn: getExportUsers,
     enabled: false,
