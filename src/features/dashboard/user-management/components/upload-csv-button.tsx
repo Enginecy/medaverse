b0779128cf4ec906c9944a1ee4f7a5b7607b4ccc
Isaac Malak <isaacmalak123@gmail.com>
2025-07-18 17:36:25 +0300

    refactor: implement UploadCSVDialog component for CSV file upload functionality

--- src/features/dashboard/user-management/components/upload-csv-button.tsx
+++ src/features/dashboard/user-management/components/upload-csv-button.tsx
@@ -1,23 +1,37 @@
 "use client";
-
 import { Button } from "@/components/ui/button";
-import { Download, Upload } from "lucide-react";
-import { generalExport } from "@/utils/exporting";
-import { getExportUsers } from "@/features/dashboard/user-management/server/db/user-management";
-import { useQuery } from "@tanstack/react-query";
-import { showSonnerToast } from "@/lib/react-utils";
-import { PulseMultiple } from "react-svg-spinners";
-import React from "react";
+import { Upload } from "lucide-react";
+import { useShowDialog } from "@/lib/react-utils";
+import React  from "react";
+import { UploadCSVDialog } from "@/components/upload-csv-dialog";
 
 export function UploadCSVButton() {
-  function uploadFile() {}
-
+  const showDialog = useShowDialog();
+  function handleUploading() {
+    showDialog((resolve) => {
+      return (
+        <UploadCSVDialog
+          title={"Upload CSV File"}
+          content={"Please upload a CSV file to import Users data."}
+          onSubmit={function (variables: unknown): Promise<unknown> {
+            throw new Error("Function not implemented.");
+          }}
+          onCancel={function (): void {
+            throw new Error("Function not implemented.");
+          }}
+          variables={undefined}
+        />
+      );
+    });
+  }
   return (
-    <Button className="w-35" variant="outline" asChild>
-      <label>
-        <span>Upload CSV</span>
-        <Upload />
-        <input className="" type="file" onChange={uploadFile}  />
+    <Button className="w-35" variant="outline" onClick={handleUploading}>
+      <label
+        className="hover:bg-accent flex cursor-pointer items-center gap-2
+          rounded-md px-3 py-2 transition-colors"
+      >
+        <Upload className="text-primary h-5 w-5" />
+        <span className="text-primary font-medium">Upload CSV</span>
       </label>
     </Button>
   );
