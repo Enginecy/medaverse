71b0e1073368f4a352edfdcb187dbb9a0e2b203e
Isaac Malak <isaacmalak123@gmail.com>
2025-09-04 15:03:23 +0300

    fix: uncomment BirthdayTable component in Home and CommissionCard in CommissionAndSalesCards

--- src/features/dashboard/home/components/commission-and-sales-cards.tsx
+++ src/features/dashboard/home/components/commission-and-sales-cards.tsx
@@ -5,7 +5,7 @@ import { SalesCard } from "@/features/dashboard/home/components/sales-card";
 export function CommissionAndSalesCards() {
   return (
     <div className="flex w-full flex-col items-center gap-2 md:flex-row">
-      {/* <CommissionCard title="Commission" range={{ min: 4200, max: 12000 }} /> */}
+      <CommissionCard title="Commission" range={{ min: 4200, max: 12000 }} />
       <SalesCard />
     </div>
   );
